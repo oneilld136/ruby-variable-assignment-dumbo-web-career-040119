529c4cb12c152db6fdfc21cf4a46ea4122ec022d
oneilld136 <oneilld136@gmail.com>
2019-03-19 13:40:21 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,6 @@
 def
   greeting="Hello World"
-  "Hello World"
   end
 
-end # Define a local variable called 'greeting',
+ # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
